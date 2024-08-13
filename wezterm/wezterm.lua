735004bf7f7587e7b937b670eb9e790c2cdbfc16
OxKahi <waifumaximalist@gmail.com>
2024-08-13 22:06:12 +0800

    adjusted window padding and added hyperlink rules

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,6 +13,16 @@ config.colors = cyberpunk.colors
 
 -- apply window Frame
 config.window_frame = cyberpunk.window_frame
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 30
+config.window_decorations = 'RESIZE'
+config.adjust_window_size_when_changing_font_size = false
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 5,
+  bottom = 1,
+}
 
 config.default_cursor_style = 'BlinkingBar'
 
@@ -20,22 +30,10 @@ config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
 config.font_size = 14.0
 config.line_height = 1.05
 -- config.bold_brightens_ansi_colors = true
-config.freetype_load_target = 'Normal'
-
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 30
-config.window_decorations = 'RESIZE'
-config.adjust_window_size_when_changing_font_size = false
-config.window_padding = {
-  left = 3,
-  right = 3,
-  top = 5,
-  bottom = 0,
-}
+-- config.freetype_load_target = 'Normal'
 
 config.check_for_updates = false
 config.automatically_reload_config = true
-
 config.default_cwd = wezterm.home_dir
 
 -- Override the CMD+T keybinding to always open in the home directory
@@ -57,6 +55,44 @@ config.mouse_bindings = {
     event = { Up = { streak = 1, button = 'Left' } },
     mods = 'CMD',
     action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
+
+config.hyperlink_rules = {
+  -- Matches: a URL in parens: (URL)
+  {
+    regex = '\\((\\w+://\\S+)\\)',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Matches: a URL in brackets: [URL]
+  {
+    regex = '\\[(\\w+://\\S+)\\]',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Matches: a URL in curly braces: {URL}
+  {
+    regex = '\\{(\\w+://\\S+)\\}',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Matches: a URL in angle brackets: <URL>
+  {
+    regex = '<(\\w+://\\S+)>',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Then handle URLs not wrapped in brackets
+  {
+    regex = '[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)',
+    format = '$1',
+    highlight = 1,
+  },
+  -- implicit mailto link
+  {
+    regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
+    format = 'mailto:$0',
   },
 }
 
