4d6853d5efcb895f96419803f325ef4ee86ba48c
OxKahi <waifumaximalist@gmail.com>
2024-08-18 03:17:59 +0800

    uncommented just realised icould just do enable = false

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,27 +1,29 @@
 return {
-  -- {
-  --   'github/copilot.vim',
-  --   config = function()
-  --     -- Disable omni completion for sql @WARN: not sure if its the best idea
-  --     vim.g.ftplugin_sql_omni_key_right = ''
-  --     vim.g.ftplugin_sql_omni_key_left = ''
-  --
-  --     vim.keymap.set('i', '<right>', function()
-  --       if vim.fn['copilot#GetDisplayedSuggestion']().text ~= '' then
-  --         vim.api.nvim_feedkeys(vim.fn['copilot#Accept'](), 'i', true)
-  --         return ''
-  --       else
-  --         return '<right>'
-  --       end
-  --     end, {
-  --       expr = true,
-  --       replace_keycodes = true,
-  --       silent = true,
-  --     })
-  --
-  --     vim.g.copilot_no_tab_map = true
-  --   end,
-  -- },
+  {
+    'github/copilot.vim',
+    enabled = false,
+    config = function()
+      -- Disable omni completion for sql @WARN: not sure if its the best idea
+      vim.g.ftplugin_sql_omni_key_right = ''
+      vim.g.ftplugin_sql_omni_key_left = ''
+
+      vim.keymap.set('i', '<right>', function()
+        if vim.fn['copilot#GetDisplayedSuggestion']().text ~= '' then
+          vim.api.nvim_feedkeys(vim.fn['copilot#Accept'](), 'i', true)
+          return ''
+        else
+          return '<right>'
+        end
+      end, {
+        expr = true,
+        replace_keycodes = true,
+        silent = true,
+        desc = 'acept copilot',
+      })
+
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
   {
     'supermaven-inc/supermaven-nvim',
     config = function()
@@ -49,6 +51,7 @@ return {
         expr = true,
         replace_keycodes = true,
         silent = true,
+        desc = 'acept supermaven',
       })
     end,
   },
