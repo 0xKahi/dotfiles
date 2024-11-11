76f0d1a2725bef7e84f623edebb5af8257964963
OxKahi <waifumaximalist@gmail.com>
2024-11-12 00:40:04 +0800

    updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -58,10 +58,7 @@ return {
           -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
         },
-
-        autopairs = {
-          enable = true,
-        },
+        autopairs = { enable = true },
       })
     end,
   },
