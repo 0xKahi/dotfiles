f68b53137d974c0a30dcd4e98f219227f0f1591e
OxKahi <waifumaximalist@gmail.com>
2025-06-11 14:01:45 +1000

    added twilight and light switch plugins

---
+++ nvim/lua/plugins/misc/init.lua
@@ -0,0 +1,4 @@
+return {
+  require('plugins.misc.light-switch'),
+  require('plugins.misc.twilight'),
+}
---
+++ nvim/lua/plugins/misc/light-switch.lua
@@ -0,0 +1,35 @@
+return {
+  'markgandolfo/lightswitch.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  keys = { { '<leader>ls', ':LightSwitchShow<CR>', desc = '[L]ight [S]witch', noremap = true, silent = true } },
+  config = function()
+    require('lightswitch').setup({
+      toggles = {
+        {
+          name = 'Formatting',
+          enable_cmd = 'FormatToggle!',
+          disable_cmd = 'FormatToggle!',
+          state = false,
+        },
+        {
+          name = 'Twilight',
+          enable_cmd = 'TwilightEnable',
+          disable_cmd = 'TwilightDisable',
+          state = false, -- Initially disabled
+        },
+        {
+          name = 'Copilot',
+          enable_cmd = 'Copilot enable',
+          disable_cmd = 'Copilot disable',
+          state = true, -- Initially enabled
+        },
+        {
+          name = 'LSP',
+          enable_cmd = ':LspStart<CR>',
+          disable_cmd = ':LspStop<CR>',
+          state = true,
+        },
+      },
+    })
+  end,
+}
