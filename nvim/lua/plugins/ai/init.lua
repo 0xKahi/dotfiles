277f1a6255e3569eab75d066279b472c294ca977
OxKahi <waifumaximalist@gmail.com>
2025-08-21 02:08:19 +0800

    remove augment

--- nvim/lua/plugins/ai/init.lua
+++ nvim/lua/plugins/ai/init.lua
@@ -3,6 +3,5 @@ return {
   require('plugins.ai.mcp-hub'),
   require('plugins.ai.copilot'),
   require('plugins.ai.opencode'),
-  -- require('plugins.ai.augment'),
   -- require('plugins.ai.code-companion'),
 }
