606c0dfa87041a415c89f21cd3e6b6cf46b3a04d
OxKahi <waifumaximalist@gmail.com>
2025-09-15 02:20:25 +0800

    updated neovim plugins and fzf config

--- zsh/configs/fzf-config.zsh
+++ zsh/configs/fzf-config.zsh
@@ -2,24 +2,26 @@
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
   --highlight-line \
   --info=inline-right \
+  --height=25%\
   --ansi \
   --layout=reverse \
   --border \
   --color=bg:-1 \
   --color=bg+:#311b92 \
   --color=border:#00FF9C \
+  --color=label:#ED7892 \
   --color=fg:#c8d3f5 \
   --color=gutter:#1e2030 \
-  --color=header:#FDF980 \
+  --color=header:#D498F8 \
   --color=hl+:#55BBF9 \
   --color=hl:#55BBF9 \
   --color=info:#545c7e \
-  --color=marker:#ED7892 \
-  --color=pointer:#ED7892 \
-  --color=prompt:#D498F8 \
+  --color=marker:#fca7ea \
+  --color=pointer:#fca7ea \
+  --color=prompt:#ED7892 \
   --color=query:#c8d3f5:regular \
   --color=scrollbar:#55BBF9 \
-  --color=separator:#00FF9C \
+  --color=separator:#FDF980 \
   --color=spinner:#ED7892 \
 "
 
