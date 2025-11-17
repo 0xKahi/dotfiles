0573dd2d3b3ca06f3c3e774acad41457150ffd84
OxKahi <waifumaximalist@gmail.com>
2025-11-17 23:52:04 +0800

    use custom arrow plugin

--- nvim/lua/plugins/arrow.lua
+++ nvim/lua/plugins/arrow.lua
@@ -1,7 +1,10 @@
 return {
-  'otavioschwanck/arrow.nvim',
+  '0xKahi/arrow.nvim',
+  -- dir = '~/Desktop/code/editor_extras/vim_plugins/arrow.nvim',
+  -- dev = true,
   opts = {
     show_icons = true,
+    icon_provider = 'mini',
     leader_key = '<leader>ao', -- Recommended to be a single key
     buffer_leader_key = '<leader>m', -- Per Buffer Mappings
 
@@ -19,8 +22,8 @@ return {
       remove = 'x', -- only used if separate_save_and_remove is true
       next_item = ']',
       prev_item = '[',
+      toggle_bookmark_type = '<TAB>',
     },
-
     -- controls the appearance and position of an arrow window (see nvim_open_win() for all options)
     window = {
       width = 'auto',
@@ -28,6 +31,13 @@ return {
       row = 'auto',
       col = 'auto',
       border = 'rounded',
+    },
+
+    dir_bookmark_config = {
+      open_action = function(dir_path, _)
+        -- print('Opening dir:', dir_path)
+        require('oil').open_float(dir_path)
+      end,
     },
     separate_save_and_remove = true, -- if true, will remove the toggle and create the save/remove keymaps.
   },
