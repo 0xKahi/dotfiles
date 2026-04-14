eed4cb27aa8e2051ec4eae4f8a4f14c8fc619739
kahi <waifumaximalist@gmail.com>
2026-04-15 05:33:54 +0800

    moved unused plugins to archive folder

--- nvim/lua/plugins/misc/init.lua
+++ nvim/lua/plugins/misc/init.lua
@@ -1,8 +1,5 @@
 return {
-  -- require('plugins.misc.light-switch'),
-  -- require('plugins.misc.twilight'),
-  -- require('plugins.misc.typr'),
-  -- require('plugins.misc.bloat'),
+  require('plugins.misc.bloat'),
   require('plugins.misc.screenkey'),
   require('plugins.misc.wrapped'),
 }
